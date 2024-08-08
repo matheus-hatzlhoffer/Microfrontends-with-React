422dd12ab2377f63392726656544daa1c7020fc1
Matheus Carvalho Hatzlhoffer <matheus.a.carvalho@ericsson.com>
2024-08-07 21:12:52 -0300

    integrating both projects

--- different-tech/shell/config/webpack.dev.js
+++ different-tech/shell/config/webpack.dev.js
@@ -1,5 +1,6 @@
 const { merge } = require("webpack-merge");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const commonConfig = require("./webpack.common");
 
 const devConfig = {
@@ -13,6 +14,13 @@ const devConfig = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./public/index.html",
+    }),
+    new ModuleFederationPlugin({
+      name: "shell",
+      remotes: {
+        marketing: "marketing@http://localhost:8081/remoteEntry.js",
+      },
+      shared: ["react", "react-dom"],
     }),
   ],
 };
